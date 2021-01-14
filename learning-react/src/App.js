fe46bcd7f2ff7713888f11f88c76231b166bc268
andrerocha <wwexdx@gmail.com>
2021-01-14 15:34:26 -0100

    added  learning react project

---
+++ learning-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import HelloWorld from './Components/HelloWorld'
+
+function App() {
+  return (
+    <div>
+      <HelloWorld name="BALULA"/>
+    </div>
+  );
+}
+
+export default App;
